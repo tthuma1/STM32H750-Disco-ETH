94bd7a90855d7dee17f93b5a08149546b59f6753
Tim Thuma <tim.thuma@students.vegova.si>
2025-06-19 21:02:03 +0200

    POST works

--- server/server.js
+++ server/server.js
@@ -1,38 +1,49 @@
+// Complete file: server.js
+const net = require("net");
 const express = require("express");
-const bodyParser = require("body-parser");
-const http = require("http");
-const socketIo = require("socket.io");
 
+let latestTemp = null;
+
+// Raw TCP server for STM32
+const tcpServer = net.createServer((socket) => {
+  socket.on("data", (data) => {
+    const req = data.toString();
+    const jsonStart = req.indexOf("{");
+    const jsonEnd = req.indexOf("}") + 1;
+    const jsonString = req.substring(jsonStart, jsonEnd);
+
+    try {
+      const json = JSON.parse(jsonString);
+      latestTemp = json.temp;
+      console.log("Parsed Temp:", latestTemp);
+    } catch (err) {
+      console.error("JSON parse error:", err.message);
+    }
+
+    socket.write("HTTP/1.1 200 OK\r\nContent-Length: 2\r\n\r\nOK");
+  });
+});
+
+tcpServer.listen(12345, () => {
+  console.log("TCP Server (STM32) running on port 12345");
+});
+
+// Express server for browser
 const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
-
-let currentTemp = null;
-
-app.use(bodyParser.json());
-app.use(express.static("public")); // serve index.html
-
-app.post("/temperature", (req, res) => {
-  console.log(req.body)
-  const { temp } = req.body;
-  if (temp !== undefined) {
-    console.log("Received temp:", temp);
-    currentTemp = temp;
-    io.emit("updateTemp", temp); // send to all connected clients
-    res.sendStatus(200);
-  } else {
-    res.status(400).send("Invalid payload");
-  }
+app.get("/", (req, res) => {
+  res.send(`
+    <html>
+      <head><title>STM32 Temp</title></head>
+      <body style="font-family:sans-serif;text-align:center;margin-top:100px;">
+        <h1>STM32 Temperature</h1>
+        <div style="font-size:3em;color:#2a9d8f;">
+          ${latestTemp !== null ? latestTemp + " °C" : "--"}
+        </div>
+      </body>
+    </html>
+  `);
 });
 
-io.on("connection", (socket) => {
-  console.log("Web client connected");
-  if (currentTemp !== null) {
-    socket.emit("updateTemp", currentTemp);
-  }
-});
-
-const PORT = 12345;
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.listen(3000, () => {
+  console.log("Web interface running at http://localhost:3000");
 });
